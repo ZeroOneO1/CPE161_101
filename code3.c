b17ea2b7788378ebf046ca3421129c854cf20a3a
ZeroOne <galaxypop.sa01@gmail.com>
2022-12-05 20:35:22 +0700

    Show radius area

---
+++ code3.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+#include<conio.h> 
+main() 
+{
+ float radius, area; 
+ radius = 1.0; 
+ area = 3.14159 * radius * radius; 
+ printf("Area = %f", area); 
+ getch(); 
+ return 0; 
+}
+//โค้ดแสดงพื้นที่รัศมี
